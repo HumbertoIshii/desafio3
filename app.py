a1a398590201bfb46411aa57d19cc52d61455089
Humberto <humbertosjc@hotmail.com>
2024-04-24 20:59:28 -0300

    Atualização geral no desenvolvimento do desafio (correção de bug, adição de conteudo na home e loja, criação das paginas dos sistemas)

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ def index():
 def loja():
     return render_template('loja.html')
 
+@app.route('/dnd')
+def dnd():
+    return render_template('dnd.html')
+
+@app.route('/ordem')
+def ordem():
+    return render_template('ordem.html')
+
+@app.route('/cthulhu')
+def cthulhu():
+    return render_template('cthulhu.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
